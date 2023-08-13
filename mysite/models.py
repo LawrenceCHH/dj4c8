6d2982d5e0df69fa85a25a3dd8da8fc401b85f01
LawrenceCHH <lawrencechh.cs@gmail.com>
2023-08-13 11:46:11 +0800

    modify

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Mood(models.Model):
+    status = models.CharField(max_length=10, null=False)
+    def __str__(self):
+        return self.status
+class Post(models.Model):
+    mood = models.ForeignKey('Mood', on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=10, default='不願意透漏身分的人')
+    message = models.TextField(null=False)
+    del_pad
+    # PDF P 248
--- mysite/views.py
+++ mysite/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    years = range(1960, 2024)
     try:
         urid = request.GET['user_id']
         urpass = request.GET['user_pass']
+        byear = request.GET['byear']
+        urfcolor = request.GET.getlist('fcolor')
     except:
         urid = None
     if urid != None and urpass == '12345':
